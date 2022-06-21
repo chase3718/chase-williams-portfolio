13521fb5e1376c16daac9fa1cc6bc28fd118721b
Chase Williams <chasewilliams98@gmail.com>
2022-06-21 13:22:07 -0400

    Added routing

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects />} />
-      </Routes>
+      <div className="page-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </div>
     </div>
   );
 }
