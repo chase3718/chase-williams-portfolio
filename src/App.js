2ad54d1c6b1c99e053344404069573f597a7e9f3
Chase Williams <chasewilliams98@gmail.com>
2022-06-26 23:42:17 -0400

    Consolidated components to single file, Added banner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import { Routes, Route, Link } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
+import React from 'react'
+import { Banner, ContentDivider } from './components/components.js'
 import './App.css'
-import UnderConstruction from "./components/UnderConstruction";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="page-content">
-        {/* <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes> */}
-        <UnderConstruction />
-      </div>
+    <div className="app">
+      <Banner />
     </div>
-  );
+  )
 }
-
-export default App;
