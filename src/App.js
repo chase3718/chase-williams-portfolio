ad7f3157822e4ed593c7b716cb51261c8c70a1ff
Chase Williams <49654824+chase3718@users.noreply.github.com>
2022-06-28 12:07:02 -0400

    Added routing with useNavigate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Banner, ContentDivider } from './components/components.js'
+import { Router } from 'react-router-dom'
 import './App.css'
 
 export default function App() {
--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import './styles.css';
 import { ContentDivider } from '../components.js';
+import { useNavigate } from 'react-router-dom';
 
 export default function Index() {
 
     const [sticky, setSticky] = useState("");
+
+    // Using useNavigate to navigate to the next page instead of Link for better styling
+    const nav = useNavigate();
+    const handleNavigate = (to) => {
+        nav(to);
+    };
 
     // Set the sticky state to true when the window is scrolled past the banner
     useEffect(() => {
@@ -17,7 +24,13 @@ export default function Index() {
     const isSticky = () => {
         // Method that will fix header after a specific scrollable
         const scrollTop = window.scrollY;
-        const stickyClass = scrollTop >= 250 ? "is-sticky" : "";
+        let stickyClass = "";
+        // const stickyClass = scrollTop >= 250 ? "is-sticky" : "";
+        if (sticky !== "is-sticky" && scrollTop >= 250) {
+            stickyClass = "is-sticky";
+        } else if (sticky === "is-sticky" && scrollTop > 0) {
+            stickyClass = "is-sticky";
+        }
         setSticky(stickyClass);
     };
 
@@ -25,20 +38,20 @@ export default function Index() {
 
     return (
         <div className={classes}>
-            <div className="title">
+            <button onClick={() => handleNavigate('/')} className="button-title">
                 <h3>Chase Williams</h3>
                 <div className="spacer"></div>
                 <div className="logo"></div>
-            </div>
+            </button>
             <ContentDivider className="horizontal" />
             <div className="button-container">
-                <button className="button resume">
+                <button onClick={() => handleNavigate('/resume')} className="button resume">
                     <h3>Resume</h3>
                 </button>
-                <button className="button resume">
+                <button onClick={() => handleNavigate('/portfolio')} className="button resume">
                     <h3>Portfolio</h3>
                 </button>
-                <button className="button resume">
+                <button onClick={() => handleNavigate('/contact')} className="button resume">
                     <h3>Contact</h3>
                 </button>
             </div>
