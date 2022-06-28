98ea6935595ad1a526c0f465c4a40a5a2f9837f3
Chase Williams <49654824+chase3718@users.noreply.github.com>
2022-06-28 11:07:40 -0400

    Added sticky banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
 import { ContentDivider } from '../components.js';
 
-export default function hep() {
+export default function Index() {
+
+    const [sticky, setSticky] = useState("");
+
+    // Set the sticky state to true when the window is scrolled past the banner
+    useEffect(() => {
+        window.addEventListener("scroll", isSticky);
+        return () => {
+            window.removeEventListener("scroll", isSticky);
+        };
+    }, []);
+
+    const isSticky = () => {
+        // Method that will fix header after a specific scrollable
+        const scrollTop = window.scrollY;
+        const stickyClass = scrollTop >= 250 ? "is-sticky" : "";
+        setSticky(stickyClass);
+    };
+
+    let classes = "banner " + sticky;
+
     return (
-        <div className="banner"> 
+        <div className={classes}>
             <div className="title">
                 <h3>Chase Williams</h3>
                 <div className="spacer"></div>
                 <div className="logo"></div>
             </div>
-            <ContentDivider className="horizontal"/>
+            <ContentDivider className="horizontal" />
             <div className="button-container">
                 <button className="button resume">
                     <h3>Resume</h3>
